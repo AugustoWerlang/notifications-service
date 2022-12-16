84b1caffbd11d7851b345d646b2d5ee02d8ff7a5
Augusto <werlang10@gmail.com>
2022-12-16 09:49:43 -0300

    aula 2 terminada

--- src/application/use-cases/send-notification.spec.ts
+++ src/application/use-cases/send-notification.spec.ts
@@ -1,26 +1,20 @@
 import { SendNotification } from "./send-notification";
-import { Notification } from "../entities/notification";
-
-const notifications: Notification[] = [];
-
-const notificationsRepository = {
-  async create(notification: Notification) {
-    notifications.push(notification);
-  },
-}
+import { InMemoryNotificationsRepository } from "../../../test/repositories/in-memory-notifications-repository";
 
 describe("Send notification", () => {
 
   it("should be able to send a notification", async () => {
+    const notificationsRepository = new InMemoryNotificationsRepository();
     const sendNotification = new SendNotification(notificationsRepository);
 
-    await sendNotification.execute({
+    const { notification } = await sendNotification.execute({
       content: "This is a notification",
       category: "social",
       recipientId: "example-recipient-id",
     });
 
-    expect(notifications).toHaveLength(1);
+    expect(notificationsRepository.notifications).toHaveLength(1);
+    expect(notificationsRepository.notifications[0]).toEqual(notification);
   });
 
 });
